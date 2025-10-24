d233fd190868be2274a4463c6def8744162e6e35
Jacob Estes <jacob.estes@student.central.edu>
2025-10-24 09:34:07 -0500

    asdf

--- includes/03-user_auditing.sh
+++ includes/03-user_auditing.sh
@@ -132,9 +132,9 @@ ua_audit_interactive_remove_unauthorized_sudoers () {
     fi
   done
 }
-```// filepath: c:\Users\jacob\Team Script\linux-hardening-cooper\includes\03-user_auditing.sh
 ua_audit_interactive_remove_unauthorized_sudoers () {
   members="$(getent group sudo 2>/dev/null | cut -d: -f4)"
+  echo "Current sudo group members: ${members:-<none>}"
   IFS=',' read -r -a users <<< "${members:-}"
 
   for user in "${users[@]}"; do
